b5f36990a96902d0c032c65a64349c88d18a9514
Matei Andrei <andrei.matei2010@gmail.com>
2018-06-10 21:09:42 +0300

    Just a test

--- app/src/main/java/com/shelly/AccountTypeActivity.java
+++ app/src/main/java/com/shelly/AccountTypeActivity.java
@@ -3,7 +3,7 @@ package com.shelly;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AccountRegisterActivity extends AppCompatActivity {
+public class AccountTypeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
