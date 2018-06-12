5df904254e46216d9394156b1dac310fee22189f
Matei Andrei <andrei.matei2010@gmail.com>
2018-06-12 11:34:03 +0300

    AccountType functional

--- app/src/main/java/com/shelly/AccountRegisterActivity.java
+++ app/src/main/java/com/shelly/AccountRegisterActivity.java
@@ -1,10 +1,14 @@
 package com.shelly;
 
+import android.content.Intent;
+import android.graphics.drawable.LayerDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.util.HashMap;
 
 public class AccountRegisterActivity extends AppCompatActivity {
 
@@ -12,11 +16,14 @@ public class AccountRegisterActivity extends AppCompatActivity {
     View mAmbassadorView;
     View mMemberView;
     ImageView mAccountTypeIV;
+    private boolean mMemberAccountType = false;
+    private HashMap<String, String> mUserData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_register);
+        mUserData = (HashMap<String, String>) getIntent().getSerializableExtra("UserData");
 
         mAmbassadorView = findViewById(R.id.AmbassadorCardview);
         mMemberView = findViewById(R.id.MemberCardview);
@@ -25,6 +32,41 @@ public class AccountRegisterActivity extends AppCompatActivity {
         mAccountTypeIV = (ImageView) mAmbassadorView.findViewById(R.id.AccountTypeImageView);
         mAccountTypeIV.setImageDrawable(getResources().getDrawable(R.drawable.ic_ambassador));
 
+        mAmbassadorView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAmbassadorView.findViewById(R.id.SelectedType).setVisibility(View.VISIBLE);
+                LayerDrawable ld = (LayerDrawable) getResources().getDrawable(R.drawable.bg_cardview_account_type);
+                mAmbassadorView.findViewById(R.id.BackgroundConstraintLayout).setBackground(ld.findDrawableByLayerId(R.id.bgSelectedType));
+                mMemberView.findViewById(R.id.BackgroundConstraintLayout).setBackground(ld.findDrawableByLayerId(R.id.bgNotSelectedType));
+                mMemberView.findViewById(R.id.SelectedType).setVisibility(View.GONE);
+                mMemberAccountType = false;
+            }
+        });
+        mMemberView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAmbassadorView.findViewById(R.id.SelectedType).setVisibility(View.GONE);
+                LayerDrawable ld = (LayerDrawable) getResources().getDrawable(R.drawable.bg_cardview_account_type);
+                mMemberView.findViewById(R.id.BackgroundConstraintLayout).setBackground(ld.findDrawableByLayerId(R.id.bgSelectedType));
+                mAmbassadorView.findViewById(R.id.BackgroundConstraintLayout).setBackground(ld.findDrawableByLayerId(R.id.bgNotSelectedType));
+                mMemberView.findViewById(R.id.SelectedType).setVisibility(View.VISIBLE);
+                mMemberAccountType = true;
+            }
+        });
+        mFinishActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mMemberAccountType) {
+                    mUserData.put("Type", "Member");
+                } else {
+                    mUserData.put("Type", "Ambassador");
+                }
+                Intent i = new Intent(AccountRegisterActivity.this, MainActivity.class);
+                i.putExtra("UserData", mUserData);
+                startActivity(i);
+            }
+        });
 
 
     }
