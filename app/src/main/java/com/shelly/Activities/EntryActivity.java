336f4bccbfa80cefc6625b4a6f7cc80cd8eed2dd
RaresScoica <40122951+RaresScoica@users.noreply.github.com>
2018-06-22 13:19:27 +0300

    entry activity and edit mode activity

---
+++ app/src/main/java/com/shelly/Activities/EntryActivity.java
@@ -0,0 +1,15 @@
+package com.shelly.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shelly.R;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
